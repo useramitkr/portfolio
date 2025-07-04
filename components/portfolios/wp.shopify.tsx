fa7f7ba46ad88a09caa404f41fd443f04814574c
Amit Kumar <useramit2016@gmail.com>
2025-07-04 14:26:37 +0530

    Wp & Shopify Portfolio - Updated

--- components/portfolios/wp.shopify.tsx
+++ components/portfolios/wp.shopify.tsx
@@ -9,7 +9,7 @@ const WpShopifyPortfolio: React.FC = () => {
         <div>
             <div className='flex flex-col md:flex-row gap-3 justify-between my-2'>
                 <div className='bg-[#f6f6f6] p-3 rounded-2xl'>
-                    <Image src='/images/portfolios/shopify/decorporium.webp' alt='BEME - Shopify' width={1200} height={700} className='rounded-2xl' />
+                    <Image src='/images/portfolios/shopify/decorporium.webp' alt='Decorporium - Shopify' width={1200} height={700} className='rounded-2xl' />
                     <div className='flex items-center py-4 gap-2'>
                         <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>Shopify</p>
                         <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>eCommerce</p>
@@ -19,12 +19,12 @@ const WpShopifyPortfolio: React.FC = () => {
                         <div>
                             <h3 className='text-[20px] md:text-2xl font-medium'>Decorporium - Shopify ecommerce website</h3>
                         </div>
-                        <div className='bg-[#354C37] text-white p-5 rounded-[50%] w-10% hover:bg-amber-300 hover:text-black transition-all duration-300 ease-in cursor-pointer'><StepForward /></div>
+                        <a href="https://decorporium.com/" target="_blank" rel="noopener noreferrer"><div className='bg-[#354C37] text-white p-5 rounded-[50%] w-10% hover:bg-amber-300 hover:text-black transition-all duration-300 ease-in cursor-pointer'><StepForward /></div></a>
                     </div>
                 </div>
 
                 <div className='bg-[#f6f6f6] p-3 rounded-2xl'>
-                    <Image src='/images/portfolios/shopify/ashley and alvis.webp' alt='BEME - Shopify' width={1200} height={700} className='rounded-2xl' />
+                    <Image src='/images/portfolios/shopify/ashley and alvis.webp' alt='Ashley & Alvis - Shopify' width={1200} height={700} className='rounded-2xl' />
                     <div className='flex items-center py-4 gap-2'>
                         <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>Shopify</p>
                         <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>eCommerce</p>
@@ -32,14 +32,76 @@ const WpShopifyPortfolio: React.FC = () => {
                     </div>
                     <div className='w-full flex items-center justify-between'>
                         <div>
-                            <h3 className='text-[20px] md:text-2xl font-medium'>Ashley and Alvis - Shopify ecommerce website</h3>
+                            <h3 className='text-[20px] md:text-2xl font-medium'>Ashley & Alvis - Shopify ecommerce website</h3>
                         </div>
-                        <div className='bg-[#354C37] text-white p-5 rounded-[50%] w-10% hover:bg-amber-300 hover:text-black transition-all duration-300 ease-in cursor-pointer'><StepForward /></div>
+                        <a href="https://ashleyandalvis.com/" target="_blank" rel="noopener noreferrer"><div className='bg-[#354C37] text-white p-5 rounded-[50%] w-10% hover:bg-amber-300 hover:text-black transition-all duration-300 ease-in cursor-pointer'><StepForward /></div></a>
                     </div>
                 </div>
             </div>
 
-            
+            <div className='flex flex-col md:flex-row gap-3 justify-between my-2'>
+                <div className='bg-[#f6f6f6] p-3 rounded-2xl'>
+                    <Image src='/images/portfolios/wordpress/amvishaa.webp' alt='Amvisha - WordPress' width={1200} height={700} className='rounded-2xl' />
+                    <div className='flex items-center py-4 gap-2'>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>WordPress</p>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>Portfolio</p>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>Elementor</p>
+                    </div>
+                    <div className='w-full flex items-center justify-between'>
+                        <div>
+                            <h3 className='text-[20px] md:text-2xl font-medium'>Amvisha - Real Estate WordPress Website</h3>
+                        </div>
+                        <a href="https://amvisha.com/" target="_blank" rel="noopener noreferrer"><div className='bg-[#354C37] text-white p-5 rounded-[50%] w-10% hover:bg-amber-300 hover:text-black transition-all duration-300 ease-in cursor-pointer'><StepForward /></div></a>
+                    </div>
+                </div>
+
+                <div className='bg-[#f6f6f6] p-3 rounded-2xl'>
+                    <Image src='/images/portfolios/wordpress/feroall.webp' alt='Feroall - WordPress' width={1200} height={700} className='rounded-2xl' />
+                    <div className='flex items-center py-4 gap-2'>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>WordPress</p>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>WooCommerce</p>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>Elementor</p>
+                    </div>
+                    <div className='w-full flex items-center justify-between'>
+                        <div>
+                            <h3 className='text-[20px] md:text-2xl font-medium'>Feroall Cookware - WordPress WooCommerce Website</h3>
+                        </div>
+                        <a href="https://www.feroallcookware.com/" target="_blank" rel="noopener noreferrer"><div className='bg-[#354C37] text-white p-5 rounded-[50%] w-10% hover:bg-amber-300 hover:text-black transition-all duration-300 ease-in cursor-pointer'><StepForward /></div></a>
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex flex-col md:flex-row gap-3 justify-between my-2'>
+                <div className='bg-[#f6f6f6] p-3 rounded-2xl'>
+                    <Image src='/images/portfolios/wordpress/ilovehaldiram.webp' alt='I Love HAldiram - WordPress' width={1200} height={700} className='rounded-2xl' />
+                    <div className='flex items-center py-4 gap-2'>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>WordPress</p>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>WooCommerce</p>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>Elementor</p>
+                    </div>
+                    <div className='w-full flex items-center justify-between'>
+                        <div>
+                            <h3 className='text-[20px] md:text-2xl font-medium'>I Love Haldiram - WooCommerce WordPress Website</h3>
+                        </div>
+                        <a href="https://ilovehaldiram.com/" target="_blank" rel="noopener noreferrer"><div className='bg-[#354C37] text-white p-5 rounded-[50%] w-10% hover:bg-amber-300 hover:text-black transition-all duration-300 ease-in cursor-pointer'><StepForward /></div></a>
+                    </div>
+                </div>
+
+                <div className='bg-[#f6f6f6] p-3 rounded-2xl'>
+                    <Image src='/images/portfolios/wordpress/mideltaa.webp' alt='Midelta - WordPress' width={1200} height={700} className='rounded-2xl' />
+                    <div className='flex items-center py-4 gap-2'>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>WordPress</p>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>Products</p>
+                        <p className='bg-amber-300 py-2 px-5 rounded-3xl font-medium text-[14px]'>Elementor</p>
+                    </div>
+                    <div className='w-full flex items-center justify-between'>
+                        <div>
+                            <h3 className='text-[20px] md:text-2xl font-medium'>Midelta International - WordPress Website</h3>
+                        </div>
+                        <a href="https://www.midelta.in/" target="_blank" rel="noopener noreferrer"><div className='bg-[#354C37] text-white p-5 rounded-[50%] w-10% hover:bg-amber-300 hover:text-black transition-all duration-300 ease-in cursor-pointer'><StepForward /></div></a>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     </>
