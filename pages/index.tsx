face6064be2367bfebc559505fc2be144d57bd0f
Amit Kumar <useramit2016@gmail.com>
2025-07-04 17:13:47 +0530

    SEO Metadata: Updated.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import AboutSection from "@/components/home/aboutSection";
 import PortfolioSection from "@/components/home/portfolioSection";
 import FaqSection from "@/components/home/faqSection";
 import FlightSearch from "@/components/home/temp2";
+import Head from 'next/head';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,13 +20,29 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div className="overflow-hidden">
-      {/* <FlightSearch /> */}
-      <HeroSection />
-      <ServiceSection />
-      <AboutSection />
-      <PortfolioSection />
-      <FaqSection />
-    </div>
+    <>
+      <section>
+        <Head>
+          <title>Amit Kumar — Frontend Developer Portfolio</title>
+          <meta name="description" content="Explore the portfolio of Amit Kumar, a passionate Frontend Developer based in India. Specialized in React, Next.js, Tailwind CSS, and building modern, responsive websites." />
+          <meta name="keywords" content="Amit Kumar, Frontend Developer in India, React Developer, Next js Developer" />
+          <meta name="author" content="Amit Kumar" />
+          <meta property="og:title" content="Amit Kumar — Frontend Developer Portfolio" />
+          <meta property="og:description" content="Explore the portfolio of Amit Kumar, a passionate Frontend Developer based in India. Specialized in React, Next.js, Tailwind CSS, and building modern, responsive websites." />
+          <meta property="og:url" content="https://www.linkedin.com/in/useramitkr/" />
+          <meta property="og:site_name" content="Amit Kumar - Portfolio" />
+          <meta property="og:type" content="website" />
+        </Head>
+
+        <main className="overflow-hidden">
+          {/* <FlightSearch /> */}
+          <HeroSection />
+          <ServiceSection />
+          <AboutSection />
+          <PortfolioSection />
+          <FaqSection />
+        </main>
+      </section>
+    </>
   );
 }
